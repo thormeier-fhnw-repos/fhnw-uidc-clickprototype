4ce5481bba15f12fc449977df21f34113af38e89
Pascal Thormeier <pascal.thormeier@gmail.com>
2016-05-22 14:42:26 +0200

    Add tracking via sliders and graph

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,25 @@
 var defaultPage = 'page-login';
 //var defaultPage = 'page-loading';
 //var defaultPage = 'page-dashboard';
-//var defaultPage = 'page-track';
+//var defaultPage = 'page-track-mic';
+//var defaultPage = 'page-track-sliders';
 //var defaultPage = 'page-track-confirm';
+//var defaultPage = 'page-track-graph';
 
 $(document).ready(function () {
     $('.page').addClass('hidden');
     $('#' + defaultPage).removeClass('hidden');
 
-    $('[data-target]').click(function() {
+    $('[data-target]').click(function(e) {
         $('.page').addClass('hidden');
         $('#' + $(this).data('target')).removeClass('hidden');
+        e.stopPropagation();
+    });
+
+    $('body').click(function (e) {
+        if (!$(e.target).attr('data-target')) {
+            $('[data-target]').fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        }
+        e.stopPropagation();
     });
 });
